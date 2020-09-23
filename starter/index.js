02ba6e1faacab1265b88990378c06a5798d959df
github username <rinerkyle95@gmail.com>
2020-09-23 13:06:41 -0500

    Setting up templates in HTML

--- starter/index.js
+++ starter/index.js
@@ -36,22 +36,35 @@ const url = require('url');
 
 ////////////////////////////////////////////
 // SERVER
+console.log(__dirname)
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+
 const dataObject = JSON.parse(data)
 
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
     const pathName = req.url
 
+    // Overview page
     if (pathName === '/' || pathName === '/overview' ) {
-        res.end('This is the overview');
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.end(tempOverview);
+
+    // Product page
     } else if (pathName === '/product') {
         res.end('This is the product');
+
+    // API
     } else if (pathName === '/api') {
         res.writeHead(200, {'Content-type': 'application/json'});
         res.end(data)
+
+    // Not found
     } else {
         res.writeHead(404, {'Content-type': 'text/html'});
         res.end('<h1>This page could not be found.</h1>')
