ecdeee3f86a0381070839b2dd4071e77eab1460a
github username <rinerkyle95@gmail.com>
2020-09-23 12:39:09 -0500

    Building a simple API

--- starter/index.js
+++ starter/index.js
@@ -37,22 +37,25 @@ const url = require('url');
 ////////////////////////////////////////////
 // SERVER
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObject = JSON.parse(data)
+
+
 const server = http.createServer((req, res) => {
     console.log(req.url);
-
     const pathName = req.url
 
     if (pathName === '/' || pathName === '/overview' ) {
-        res.end('This is the overview')
+        res.end('This is the overview');
     } else if (pathName === '/product') {
-        res.end('This is the product')
+        res.end('This is the product');
+    } else if (pathName === '/api') {
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data)
     } else {
-        res.writeHead(404, {
-            'Content-type': 'text/html'
-        });
+        res.writeHead(404, {'Content-type': 'text/html'});
         res.end('<h1>This page could not be found.</h1>')
     }
-
 });
 
 server.listen(8000, '127.0.0.1', () => {
