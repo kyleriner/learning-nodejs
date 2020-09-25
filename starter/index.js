f1a221ac26c6f1372b049cbbd23fc3e9c64b7f13
github username <rinerkyle95@gmail.com>
2020-09-25 14:22:41 -0500

    Filling Tempaltes Completed

--- starter/index.js
+++ starter/index.js
@@ -36,7 +36,21 @@ const url = require('url');
 
 ////////////////////////////////////////////
 // SERVER
-console.log(__dirname)
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) {output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');}
+
+    return output;
+    
+}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
@@ -53,7 +67,12 @@ const server = http.createServer((req, res) => {
     // Overview page
     if (pathName === '/' || pathName === '/overview' ) {
         res.writeHead(200, {'Content-type': 'text/html'});
-        res.end(tempOverview);
+
+        const cardsHtml = dataObject.map(el => replaceTemplate(tempCard, el)).join('');
+        console.log(cardsHtml)
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+        res.end(output);
 
     // Product page
     } else if (pathName === '/product') {
