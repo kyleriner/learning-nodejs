cd2c7c1c919e3f577f9934ead96a8536c613502d
github username <rinerkyle95@gmail.com>
2020-09-25 14:47:00 -0500

    Parsing URL Variables complete

--- starter/index.js
+++ starter/index.js
@@ -41,7 +41,7 @@ const replaceTemplate = (temp, product) => {
     output = output.replace(/{%IMAGE%}/g, product.image);
     output = output.replace(/{%PRICE%}/g, product.price);
     output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%NUTRITION%}/g, product.nutrients);
     output = output.replace(/{%QUANTITY%}/g, product.quantity);
     output = output.replace(/{%DESCRIPTION%}/g, product.description);
     output = output.replace(/{%ID%}/g, product.id);
@@ -62,24 +62,30 @@ const dataObject = JSON.parse(data)
 
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url
+
+    const {query, pathname} = url.parse(req.url, true)
+    console.log(pathname)
 
     // Overview page
-    if (pathName === '/' || pathName === '/overview' ) {
+    if (pathname === '/' || pathname === '/overview' ) {
         res.writeHead(200, {'Content-type': 'text/html'});
 
         const cardsHtml = dataObject.map(el => replaceTemplate(tempCard, el)).join('');
-        console.log(cardsHtml)
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
 
         res.end(output);
 
     // Product page
-    } else if (pathName === '/product') {
-        res.end('This is the product');
+    } else if (pathname === '/product') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+
+        const product = dataObject[query.id]
+        const output = replaceTemplate(tempProduct, product);
+
+        res.end(output);
 
     // API
-    } else if (pathName === '/api') {
+    } else if (pathname === '/api') {
         res.writeHead(200, {'Content-type': 'application/json'});
         res.end(data)
 
